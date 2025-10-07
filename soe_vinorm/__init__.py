695cf01e449ba1f31cf5baf4d4281877dcd7e4ae
vinhdq842 <quangvinh0842@gmail.com>
2025-10-07 16:36:54 +0700

    bump version to 0.3.1

--- soe_vinorm/__init__.py
+++ soe_vinorm/__init__.py
@@ -4,7 +4,7 @@ Soe Vinorm: Vietnamese Text Normalization Toolkit
 A Python library for converting Vietnamese text to its spoken form.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 __author__ = "Vinh Dang"
 __email__ = "quangvinh0842@gmail.com"
 
